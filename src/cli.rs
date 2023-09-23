6a5ae762f625a53d9aa786dd61f799fd3ec541df
PotatoMaaan <60669730+PotatoMaaan@users.noreply.github.com>
2023-09-23 21:09:54 +0200

    add files

---
+++ src/cli.rs
@@ -0,0 +1,62 @@
+use clap::{Parser, Subcommand, ValueEnum};
+
+use libskiller::{self, Color};
+
+/// Skiller Pro+ cli
+#[derive(Parser)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Subcommands,
+
+    /// When setting a value, specifies the profile that value applies to
+    #[arg(short, long, default_value_t = 1)]
+    pub profile: u8,
+}
+
+#[derive(Subcommand)]
+pub enum Subcommands {
+    /// Set the color of the keyboard
+    Color {
+        /// The color to be set
+        color: Color,
+    },
+
+    /// Set brightness and effects (and color because the way this gets set is weird)
+    Brightness {
+        #[command(subcommand)]
+        brightness: libskiller::Brightness,
+    },
+
+    /// Change the profile to the one provided through the root level profile option
+    Profile,
+
+    /// Control if the windows key is enabled or disabled
+    WinKey { enable: WinKeyState },
+
+    /// Sets the GLOBAL (not per profile) polling rate of the keyboard
+    PollingRate { rate: u32 },
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+pub enum WinKeyState {
+    On,
+    Off,
+}
+
+impl WinKeyState {
+    pub fn to_bool(&self) -> bool {
+        match self {
+            WinKeyState::On => true,
+            WinKeyState::Off => false,
+        }
+    }
+}
+
+impl ToString for WinKeyState {
+    fn to_string(&self) -> String {
+        match self {
+            WinKeyState::On => "On".to_string(),
+            WinKeyState::Off => "Off".to_string(),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,124 @@
+use std::{process::exit, time::Duration};
+
+use clap::Parser;
+use cli::Cli;
+use libskiller::{Brightness, SkillerProPlus};
+
+mod cli;
+
+fn main() {
+    let args = Cli::parse();
+
+    if (args.profile == 0) || (args.profile > 3) {
+        eprintln!("Profile can only be 1, 2 or 3");
+        exit(1);
+    }
+    let p = num_to_profile_unchecked(args.profile);
+
+    let skiller = match SkillerProPlus::new(Duration::from_secs(1)) {
+        Err(e) => {
+            print_rusb_error(&e);
+            exit(1);
+        }
+        Ok(skiller) => {
+            match skiller {
+                Some(s) => s,
+                None => {
+                    eprintln!("No Skiller Pro+ device found. Make sure the keyboard is connected to your pc.");
+                    exit(1);
+                }
+            }
+        }
+    };
+
+    let command_result = match args.command {
+        cli::Subcommands::Color { color } => {
+            println!("Setting Profile {:?} to {:?}.", p, color);
+            skiller.set_color(color, p)
+        }
+        cli::Subcommands::Brightness { brightness } => {
+            if let libskiller::Brightness::Static { level, color: _ } = &brightness {
+                if level > &10 {
+                    eprintln!("Level must be between 0 and 10.");
+                    exit(1);
+                }
+            }
+
+            pretty_print_brightness(&brightness);
+            skiller.set_brightness(brightness, p)
+        }
+        cli::Subcommands::WinKey { enable } => {
+            println!("Setting windows key to {}.", enable.to_string());
+
+            skiller.set_win_key(enable.to_bool(), p)
+        }
+        cli::Subcommands::Profile => {
+            println!("Setting profile to {:?}", p);
+            skiller.set_profile(p)
+        }
+        cli::Subcommands::PollingRate { rate } => {
+            if let Some(rate) = try_to_polling(rate) {
+                println!("Setting polling rate to {:?}", rate);
+                skiller.set_polling_rate(rate)
+            } else {
+                eprintln!("Polling rate must be 125, 250, 500 or 1000.");
+                exit(1);
+            }
+        }
+    };
+
+    match command_result {
+        Err(err) => {
+            print_rusb_error(&err);
+            exit(1);
+        }
+        Ok(bytes_written) => {
+            if bytes_written == 0 {
+                eprintln!("Error: a command was sent but no bytes were written!");
+                exit(1);
+            }
+        }
+    }
+}
+
+fn print_rusb_error(err: &rusb::Error) {
+    match err {
+        rusb::Error::Access => {
+            eprintln!("Got libusb Access error. Consider running as root or setting a udev rule.");
+        }
+        _ => {
+            eprintln!("Libusb returned an error: {:?}.", err);
+        }
+    }
+}
+
+fn num_to_profile_unchecked(num: u8) -> libskiller::Profile {
+    match num {
+        1 => libskiller::Profile::P1,
+        2 => libskiller::Profile::P2,
+        3 => libskiller::Profile::P3,
+        _ => unreachable!(),
+    }
+}
+
+fn pretty_print_brightness(brightness: &Brightness) {
+    match brightness {
+        Brightness::Static { level, color } => {
+            println!("Setting static brightness to level {}, {:?}.", level, color)
+        }
+        Brightness::Pulsating { color } => {
+            println!("Setting pulsating brightness with color {:?}.", color)
+        }
+        Brightness::Cycle => println!("Setting brightness cycle."),
+    }
+}
+
+fn try_to_polling(num: u32) -> Option<libskiller::PollingRate> {
+    match num {
+        125 => Some(libskiller::PollingRate::HZ125),
+        250 => Some(libskiller::PollingRate::HZ250),
+        500 => Some(libskiller::PollingRate::HZ500),
+        1000 => Some(libskiller::PollingRate::HZ1000),
+        _ => None,
+    }
+}
